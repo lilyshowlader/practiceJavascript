14c663ea1c7ce5178a8bf90d00d65292c70019ef
lilyshowlader <lilyshowlader@gmail.com>
2022-12-15 14:24:35 -0500

    complete parseInt algo

--- file.js
+++ file.js
@@ -1,3 +1,134 @@
+// Create a function with two arguments that will return an array of the first n multiples of x. Assume both the given number and the number of times to count will be positive numbers greater than 0. Return the results as an array or list
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// multiply param one by param two and print out multiples in between 
+// push numbers into an array
+
+// a for loop is used when you want to execute code a certain amount of times
+// we can't use a forEach here because you can't use a forEach on an empty array
+// 
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = x; i <= x * n ; i++) {
+//       if ( i % x === 0) {
+//         z.push(i)
+//       }
+//   }
+//   return z;
+// }
+
+// console.log(countBy(2,6))
+// [2,4,6,8,10,12]
+// 2 <= 12
+// 2 % 2 === 0 // then it can be pushed in the array
+// 4 <= 12
+// 4 % 2 === 0 // pushed into the array
+// 6 <= 12
+// 6 % 2 === 0 // pushed into the array
+// 8 <= 12
+// 8 % 2 === 0 // pushed into the array
+// 10 <= 12
+// 10 % 2 === 0 // pushed into the array
+// 12 <= 12
+// 12 % 2 === 0 // pushed into the array
+// the condition has been met above  ^
+
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 2; 4 <= 2 * 6 ; i++) {
+//       if ( 4 % 2 === 0) {
+//         z.push(i)
+//       }
+//   }
+//   return z;
+// }
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 2; 6 <= 2 * 6 ; i++) {
+//       if ( 6 % 2 === 0) {
+//         z.push(i)
+//       }
+//   }
+//   return z;
+// }
+
+// SELECT newborns.mother_name, midwives.first_name FROM newborns JOIN midwives ON newborns.id = midwives.id 
+
+// I want the ID of the baby, the midwive assigned to the baby, and the years of experience for the baby
+
+// SELECT FROM JOIN ON =
+// SELECT newborns.id, midwives.first_name, midwives.years_experience FROM newborns JOIN midwives ON newborns.id = midwives.id 
+
+// let numbers = [14,3,13,24,25]
+// sum = 0
+
+// for (let i = 0; i < 4; i++) {
+//   console.log(numbers[i])
+// }
+
+// write a for loop that iterates from 0-50 and logs the numbers in the console
+
+// for (let i = 0; i <=50; i++) {
+//   console.log(i)
+// }
+
+// write a for loop that iterates from 1-20 and logs out whether each number in the range is even or odd
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} is even`)
+//   } else {
+//     console.log(`${i} is odd`)
+//   }
+// }
+
+
+
+// Our input data
+const voteArr = ['yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'yes', 'no', 'no']
+
+// voteTallyDot takes in an array
+function voteTallyDot(arr) {
+  // Create an empty object where we will accumulate votes tallies.
+  // Assign this object to a constant named tally
+  const tally = {}
+  // Iterate through the array passed into the function
+  for (let i = 0; i < arr.length; i++) {
+    // On each iteration, assign the value held at position i in the arr to vote
+    const vote = arr[i]
+    if (vote === 'yes') {
+      // if the vote is yes
+      // check to see if a yes key is defined on the tally object
+      if (tally.yes) {
+        // if it is increment its value by 1
+        tally.yes = tally.yes + 1
+      } else {
+        // if it is not define the key and give it a value of 1
+        tally.yes = 1
+      }
+    } else if (vote === 'no') {
+      // if the vote is no
+      // check to see if a no key is defined on the tally object
+      if (tally.no) {
+        // if it is, increment its value by 1
+        tally.no = tally.no + 1
+      } else {
+        // if it is not, define the key and give it a value of 1
+        tally.no = 1
+      }
+    }
+  }
+  // return tally after we have iterated over the array that was passed in
+  return tally
+}
+
+// Call and log the return value of the voteTallyDot function
+// console.log(voteTallyDot(voteArr))
+
+
+
 animals = ['cat','dog', 'cat', 'cat', 'dog']
             // i      i      i      i      i
 
@@ -11,8 +142,6 @@ function calculateAnimals(animals) {
     let value = animals[i]
   // storing all the elements in the animals array into a variable called value  
     if (value === 'cat') {
-      // I want to create a key for every new element in the array
-      // if the key already exists, I want to increment the vote by one
       if (count.cat) {
         count.cat = count.cat + 1
       } else {
@@ -30,91 +159,24 @@ function calculateAnimals(animals) {
 
 console.log(calculateAnimals(animals))
 
-// this function will return an object with the count of each character within a string 
-function charCount(string) {
-  let count = {}
-  // loop through the string. 
-  for (let i = 0; i < string.length; i++) {
-    // store the value of i in each iteration to a variable
-    let value = string[i]
-    if (count[value]) {
-      count[value]++
-    } else {
-      count[value] = 1
-    }
-  } return count 
 
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
 
-}
+// Write a program that returns the girl's age (0-9) as an integer.
 
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
+function getAge(inputString){
+  // I need the 0 index of the string
+   let age = parseInt(inputString[0])
+   return age
+ }
 
-
-// Prompt:
-
-// Write a function called addOne that takes a single number as an argument and returns that number plus 1.
-
-// Examples:
-
-// addOne(1) //=> 2
-// addOne(-5) //=> -4
-
-function addOne (number) {
-  // takes an number
-  // returns that number plus one 
-  let total = number + 1
-  return total 
-}
-
-// console.log(addOne(2))
-
-// Prompt:
-
-// - Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
-// - If either argument is not a number, return the value of NaN.
-
-// Examples:
-
-// addTwoNumbers(5, 10) //=> 15
-// addTwoNumbers(10, -2) //=> 8
-// addTwoNumbers(0, 0) //=> 0
-// addTwoNumbers('Hello', 5) //=> NaN
-
-function addTwoNumbers(numberOne, numberTwo) {
-  let total = numberOne + numberTwo
-  if (typeof numberOne === 'number' && typeof numberTwo === 'number') {
-    return numberOne + numberTwo
-  } else {
-    return NaN
+function calculateSum(arr3, arr4) {
+  let results = [];
+  for (let i = 0; i < arr3.length; i++) {
+    results.push(arr3[i] + arr4[i])
   }
-  // accepts two numbers
-  // returns the sum of those two numbers
-  // if either argument is not a number, return NaN
-  // you can figure out if something is not a number by using typeof 
-
-
-}
-
-// Prompt:
-
-// - Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
-// - If the array is empty, return 0 (zero).
-
-// Examples:
-
-// sumNumbers([10]) //=> 10
-// sumNumbers([5, 10]) //=> 15
-// sumNumbers([2, 10, -5]) //=> 7
-// sumNumbers([]) //=> 0
-
-function sumNumbers(arrayOfNumbers) {
-  let value = 0
-  for (let i = 0; i < arrayOfNumbers.length; i++) {
-    value = value + arrayOfNumbers[i]
+  return results;
   }
-  // accepts a single array of numbers
-  // returns the sum of ALL numbers in the array
-  // for loop
-  // need to store the value in a variable
-  return value 
-}
+  
